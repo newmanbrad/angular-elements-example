6dc5c35151b01c91f4081dba6aee20a0ff5ba4f8
Brad Newman <bradnewman@mac-bnewman.obs.outback.com>
2018-06-28 08:53:58 -0400

    adding notes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,30 @@ import { ExampleElementComponent } from './example-element/example-element.compo
   declarations: [
     ExampleElementComponent
   ],
+
+  /**
+   * entryComponents
+   *
+   * is an array of components which aren’t
+   * embedded in a particular template, but still created somehow imperatively.
+   */
+
   entryComponents: [
     ExampleElementComponent
   ]
 })
 export class AppModule {
   constructor(private injector: Injector) {
+
+    /**
+     * createCustomElement
+     *
+     * This function creates and
+     * returns an implementation for NgElement based on the provided component.
+     */
+
     const customElement = createCustomElement(ExampleElementComponent, { injector });
+
     customElements.define('example-element', customElement);
   }
 
